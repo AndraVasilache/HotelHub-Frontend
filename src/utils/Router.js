c618877ca7ce182d45fdbc3cd8332b6389426f3b
AndraVasilache <vasilacheandragabriela@gmail.com>
2021-05-11 22:22:29 +0300

    work in progress

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,5 +1,4 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import StartScreen from '../views/StartPage/StartScreen';
 import LoginScreen from '../views/Login/LoginScreen';
 import AboutUsScreen from '../views/AboutUs/AboutUsScreen';
@@ -7,21 +6,39 @@ import SignUpScreen from '../views/CreateAccount/SignUpScreen';
 import ReservationScreen from '../views/MakeReservation/MakeReservation';
 import DebugScreen from '../views/DebugScreen';
 
-const Navigator = createStackNavigator(
-  {
-    Start: StartScreen,
-    Login: LoginScreen,
-    AboutUs: AboutUsScreen,
-    CreateAccount: SignUpScreen,
-    MakeReservation: ReservationScreen,
-    Debug: DebugScreen,
-  },
-  {
-    initialRouteName: 'Debug',
-    defaultNavigationOptions: {
-      title: 'HotelHub',
-    },
-  },
+// const Navigator = createStackNavigator(
+//   {
+//     Start: StartScreen,
+//     Login: LoginScreen,
+//     AboutUs: AboutUsScreen,
+//     CreateAccount: SignUpScreen,
+//     MakeReservation: ReservationScreen,
+//     Debug: DebugScreen,
+//   },
+//   {
+//     initialRouteName: 'Debug',
+//     defaultNavigationOptions: {
+//       title: 'HotelHub',
+//     },
+//   },
+// );
+
+const Stack = createStackNavigator();
+
+const Router = () => (
+  <Stack.Navigator
+    initialRouteName="Home"
+    screenOptions={{ gestureEnabled: false }}
+  >
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen}
+    />
+    <Stack.Screen
+      name="Profile"
+      component={ProfileScreen}
+    />
+  </Stack.Navigator>
 );
 
-export default createAppContainer(Navigator);
+export default Router;
