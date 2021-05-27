19a46c17814d94d9d626a7e3052c28a1acf15163
AndraVasilache <vasilacheandragabriela@gmail.com>
2021-05-27 09:46:28 +0300

    asdas

--- src/views/admin/Hotel/HotelPage.js
+++ src/views/admin/Hotel/HotelPage.js
@@ -1,39 +1,38 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import axios from 'axios';
 
-function get(user) {
-  const options = {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    },
-    params: {
-      hotel_id: user.user_id,
-    },
-  };
-
-  axios.get('https://hotelhubip.herokuapp.com/admin/actions/hotel/get', options)
-    .then((response) => {
-      console.log(response);
-
-      if (response.data === '') {
-        // setIncorrectLogin(true);
-        return;
-      }
-
-      const hotel = response.data;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
 function HotelPage({ route, navigation }) {
   const user = (route && route.params && route.params.user) ? route.params.user : { email: '', password: '' };
-  console.log(user);
+  const [hotel, setHotel] = useState(undefined);
 
-  get(user);
+  function get(user) {
+    const options = {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+      },
+      params: {
+        hotel_id: user.hotel_admin,
+      },
+    };
+
+    axios.get('https://hotelhubip.herokuapp.com/admin/actions/hotel/get', options)
+      .then((response) => {
+        console.log(response);
+
+        setHotel(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  if (hotel == undefined) {
+    console.log(hotel);
+    get(user);
+    console.log(hotel);
+  }
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
